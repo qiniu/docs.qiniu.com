e750293b1e577ee99705d08c53996bb24620b438
icattlecoder <icattlecoder@gmail.com>
2013-07-17 12:18:49 +0800

    doc

--- static/js/qiniudoc.js
+++ static/js/qiniudoc.js
@@ -18,5 +18,14 @@ $(function() {
       })
     }
   }
-
+  $.scrollUp({
+	  scrollName: 'scrollUp',
+	  topDistance: '300',
+	  topSpeed: 300, 
+	  animation: 'fade',
+	  animationInSpeed: 200, 
+	  animationOutSpeed: 200,
+	  scrollText: '',
+	  activeOverlay: false
+  });
 });
