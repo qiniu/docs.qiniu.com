0f8cb87390b7e6e066baf6ce23ae3190cdb0b559
icattlecoder <icattlecoder@gmail.com>
2013-07-22 18:07:48 +0800

    fix menu js

--- static/js/qiniudoc.js
+++ static/js/qiniudoc.js
@@ -4,21 +4,21 @@ $(function() {
       helpList = $('#js-sidebar .js-topic'),
       firstOccurance = true
 
-  // hide list items at startup
   if($('body.api') && window.location){
-    var reg = /\/\/[^\/]+(\/.+)/g,
+    	var reg = /\/\/[^\/]+(\/.+)/g,
         docUrl = reg.exec(window.location.toString())
-    if(docUrl){
-      $('#js-sidebar .js-topic a').each(function(){
-        var url = $(this).attr('href').toString()
         var cleanDocUrl = docUrl[1].split('#')[0]
-        if(url.indexOf(cleanDocUrl) >= 0 && url.length == cleanDocUrl.length){
-          $(this).parent('li').addClass('disable')
+        cleanDocUrl = cleanDocUrl.split('/')[1]      
+        if(docUrl){
+          $('#js-sidebar .js-topic a').each(function(){
+            var url = $(this).attr('href').toString()        
+            if(url.indexOf(cleanDocUrl) >= 0 && url.length >= cleanDocUrl.length){
+              $(this).parent('li').addClass('disable')
+            }
+          })
         }
-      })
-    }
   }
-  $.scrollUp({
+    $.scrollUp({
 	  scrollName: 'scrollUp',
 	  topDistance: '300',
 	  topSpeed: 300, 
