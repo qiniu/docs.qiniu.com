2ec26165b1c8c107ff3218e521c8d31d6a9036be
SunLn <dengchenhua@gmail.com>
2013-12-03 14:12:16 +0800

    Revert "fix the some bug"
    This reverts commit bd7e5b597581c6d756bc8e5b0c21577222927326.

--- static/js/docs.js
+++ static/js/docs.js
@@ -13,6 +13,9 @@ $(function() {
             $(this).children("ul").each(function() {
                 //ul
                 $(this).attr("class", "panel-list level-three nav");
+                $(this).children("ul").each(function() {
+                    $(this).attr("class", "panel-list level-four nav");
+                });
             });
         });
         var href = url;
@@ -74,11 +77,6 @@ $(function() {
         return false;
     });
 
-    // API页固定侧边栏
-    $('.container.api .side-bar').hcSticky({
-        bottomEnd: -1,
-        top: 0
-    });
 
     //调整API页面容器高度，若侧边栏超高，则调整
     function adjustApiBoxHeight() {
@@ -93,11 +91,11 @@ $(function() {
         var $next = $(this).next('.panel-body');
         var $siblings = $(this).parents('.panel').siblings('.panel');
         if ($next.is(':visible')) {
-            $next.hide('fast', adjustApiBoxHeight);
+            $next.slideUp('fast', adjustApiBoxHeight);
             $(this).find('span.api_down_sprited').removeClass('api_down_sprited').addClass('api_default_sprited');
             $(this).find('a').removeClass('active');
         } else {
-            $next.show('fast', function() {
+            $next.slideDown('fast', function() {
                 var scrollTop = $(window).scrollTop() - 70;
                 var height = $('.panel-box').height();
                 var mainHeight = $('.main').height();
@@ -112,7 +110,7 @@ $(function() {
             $(this).find('a').addClass('active');
             $siblings.children('.panel-heading').find('.icon').removeClass('api_down_sprited').addClass('api_default_sprited');
             $siblings.children('.panel-heading').find('a').removeClass('active');
-            $siblings.children('.panel-body').hide('fast', adjustApiBoxHeight);
+            $siblings.children('.panel-body').slideUp(adjustApiBoxHeight);
         }
         return false;
     });
@@ -124,7 +122,7 @@ $(function() {
         var $link = $(this).siblings('a');
         var href = $link.attr('href').toLowerCase();
         if (href.indexOf(path) >= 0 && href.indexOf(fileName) >= 0) {
-            $(this).parents('.panel-heading').siblings('.panel-body').hide('fast', adjustApiBoxHeight);
+            $(this).parents('.panel-heading').siblings('.panel-body').slideUp(adjustApiBoxHeight);
             $link.removeClass('active');
             $link.siblings('.api_selected_sprited').removeClass('api_selected_sprited').addClass('api_unselect_sprited');
             $link.next('.icon').removeClass('api_down2_sprited');
@@ -139,12 +137,12 @@ $(function() {
         if (href.indexOf(path) >= 0 && href.indexOf(fileName) >= 0) {
             var $panelBody = $(this).parents('.panel-heading').siblings('.panel-body');
             if ($panelBody.is(':visible')) {
-                $panelBody.hide('fast', adjustApiBoxHeight);
+                $panelBody.slideUp(adjustApiBoxHeight);
                 $(this).removeClass('active');
                 $(this).siblings('.api_selected_sprited').removeClass('api_selected_sprited').addClass('api_unselect_sprited');
                 $(this).next('.icon').removeClass('api_down2_sprited');
             } else {
-                $(this).parents('.panel-heading').siblings('.panel-body').show('fast', adjustApiBoxHeight);
+                $(this).parents('.panel-heading').siblings('.panel-body').slideDown(adjustApiBoxHeight);
                 $(this).addClass('active');
                 $(this).siblings('.api_unselect_sprited').removeClass('api_unselect_sprited').addClass('api_selected_sprited');
                 $(this).next('.icon').addClass('api_down2_sprited');
@@ -159,11 +157,11 @@ $(function() {
             $(this).parents('.panel-body').siblings('.panel-heading').click();
             $(this).addClass('active');
             if ($(this).parents('.panel-heading').length > 0) {
-                $(this).parents('.panel-heading').siblings('.panel-body').show('fast', adjustApiBoxHeight);
+                $(this).parents('.panel-heading').siblings('.panel-body').slideDown(adjustApiBoxHeight);
                 $(this).siblings('.api_unselect_sprited').removeClass('api_unselect_sprited').addClass('api_selected_sprited');
                 $(this).next('.icon').addClass('api_down2_sprited');
             } else {
-                $(this).parents('.panel-body').show('fast', adjustApiBoxHeight);
+                $(this).parents('.panel-body').show(adjustApiBoxHeight);
                 $(this).parents('.panel-body').siblings('.panel-heading').find('.api_unselect_sprited').removeClass('api_unselect_sprited').addClass('api_selected_sprited');
                 $(this).parents('.panel-body').siblings('.panel-heading').find('span:last').addClass('api_down2_sprited');
                 $(this).parents('.panel-body').siblings('.panel-heading').find('a').addClass('active');
@@ -171,7 +169,11 @@ $(function() {
         }
     });
 
-
+    // API页固定侧边栏
+    $('.container.api .side-bar').hcSticky({
+        bottomEnd: -1,
+        top: 0
+    });
 
     // API页高亮代码
     $('pre code').each(function(i, e) {
