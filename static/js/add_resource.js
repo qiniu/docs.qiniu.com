08e4771e4cba218df789fa11bf867c8a32ee5da8
SunLn <dengchenhua@gmail.com>
2013-12-16 15:24:32 +0800

    change test url

--- static/js/add_resource.js
+++ static/js/add_resource.js
@@ -12,12 +12,15 @@ var _IE = (function() {
 var DocsAddResource = {
     show: function(type) {
         var l = location.href.split('#')[0];
-        //this.iframe.attr('src', 'https://portal.qiniu.com/zendesk/docs?type=' + type + '#' + l);
-        //this.iframeHide.attr('src', 'https://portal.qiniu.com/zendesk/docs?type=' + type + '#' + l);
+        //正式环境
+        // this.iframe.attr('src', 'https://portal.qiniu.com/zendesk/docs?type=' + type + '#' + l);
+        // this.iframeHide.attr('src', 'https://portal.qiniu.com/zendesk/docs?type=' + type + '#' + l);
         //本地测试
-        this.iframe.attr('src', 'http://192.168.2.102:8000/zendesk/docs?type=' + type + '#' + l);
-        this.iframeHide.attr('src', 'http://192.168.2.102:8000/zendesk/docs?type=' + type + '#' + l);
-
+        // this.iframe.attr('src', 'http://192.168.19.56:8000/zendesk/docs?type=' + type + '#' + l);
+        // this.iframeHide.attr('src', 'http://192.168.19.56:8000/zendesk/docs?type=' + type + '#' + l);
+        //线上测试
+        this.iframe.attr('src', 'https://portal-feature.qiniu.io/zendesk/docs?type=' + type + '#' + l);
+        this.iframeHide.attr('src', 'https://portal-feature.qiniu.io/zendesk/docs?type=' + type + '#' + l);
         this.obj.fadeIn().removeClass('hide').show();
         var dx = $(window).height() - 871;
         var half = dx / 2;
